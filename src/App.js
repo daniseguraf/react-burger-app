c9f9c2b6935b2141b13ff7c7886957fe7e76dbdb
Daniel <danielseguraf@gmail.com>
2019-08-09 23:36:36 -0500

    starting implementation of the burger builder

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 function App() {
   return (
-      <Layout>
+    <Layout>
       <BurgerBuilder />
-      </Layout>
+    </Layout>
   );
 }
 
