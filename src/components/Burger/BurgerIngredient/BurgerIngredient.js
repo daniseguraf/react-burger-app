9ea1e3dd98352b0f343b7dc89627cb83c3d7d585
Daniel <danielseguraf@gmail.com>
2019-08-09 23:57:57 -0500

    Adding a Dynamic Ingredient Component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,12 +1,39 @@
 import React from 'react'
 
+import styles from './BurgerIngredient.module.css'
+
 const burgerIngredient = (props) => {
   let ingredient = null
 
   switch (props.type) {
-    case ()
+    case ('bread-bottom'):
+      ingredient = <div className={styles.BreadBottom}></div>;
+      break;
+    case ('bread-top'):
+      ingredient = (
+        <div className={styles.BreadTop}>
+          <div className={styles.Seeds1}></div>
+          <div className={styles.Seeds2}></div>
+        </div>
+      );
+      break;
+    case ('meat'):
+      ingredient = <div className={styles.Meat}></div>;
+      break;
+    case ('cheese'):
+      ingredient = <div className={styles.Cheese}></div>;
+      break;
+    case ('salad'):
+      ingredient = <div className={styles.Salad}></div>;
+      break;
+    case ('beacon'):
+      ingredient = <div className={styles.Beacon}></div>;
+      break;
+    default:
+      ingredient= null;
   }
 
+  return ingredient
 }
 
 export default burgerIngredient
