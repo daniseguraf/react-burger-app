98f8000e5fd4b6beb2f0005407645a428d02b6ab
Daniel <danielseguraf@gmail.com>
2019-09-18 00:14:24 -0500

    Building the Checkout Container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+import Checkout from './containers/Checkout/Checkout'
+
 
 function App() {
   return (
     <Layout>
       <BurgerBuilder />
+      <Checkout />
     </Layout>
   );
 }
--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -2,16 +2,17 @@ import React from 'react'
 import Burger from './../../Burger/Burger'
 import Button from './../../UI/Button/Button'
 
+import classes from './CheckoutSummary.module.css'
+
 const checkoutSummary = (props) => {
   return (
-    <div className={}>
+    <div className={classes.CheckoutSummary}>
       <h1>We hope it states well!</h1>
-      <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+      <div style={{width: '100%', margin: 'auto'}}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType="Danger" clicked={props.cancel}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.continue}>CONTINUE</Button>
-
+      <Button type="Danger" clicked={props.cancel}>CANCEL</Button>
+      <Button type="Success" clicked={props.continue}>CONTINUE</Button>
     </div>
   )
 }
